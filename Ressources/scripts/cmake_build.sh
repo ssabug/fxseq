f7081bcec551ce1e9ed553f4fd18a7fe2de36af5
ssabug <gerype999@gmail.com>
2023-10-27 00:29:07 +0200

    Cmake update

--- Ressources/scripts/cmake_build.sh
+++ Ressources/scripts/cmake_build.sh
@@ -4,10 +4,14 @@ currentPath="$(pwd)"
 echo "Moving into build directory"
 cd "${workingPath}"
 
+echo "Making symbolic link to JUCE"
+ln -s "${HOME}/Soft/JUCE" "${HOME}/dev/fxseq/JUCE"
+
 mkdir build
 cd build
 echo "Starting build..."
-cmake ..
+cmake .. 
+make 
 
 echo "Get back to previous directory"
 
